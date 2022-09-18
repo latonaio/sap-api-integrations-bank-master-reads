1f40148f73bea7a60f507d1ca4100fc33ae49923
KahoY <kaho.y@latona.io>
2022-09-18 14:09:27 +0900

    release

---
+++ SAP_API_Caller/caller.go
@@ -0,0 +1,83 @@
+package sap_api_caller
+
+import (
+	"fmt"
+	"io/ioutil"
+	sap_api_output_formatter "sap-api-integrations-bank-master-reads/SAP_API_Output_Formatter"
+	"strings"
+	"sync"
+
+	sap_api_request_client_header_setup "github.com/latonaio/sap-api-request-client-header-setup"
+
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
+)
+
+type SAPAPICaller struct {
+	baseURL         string
+	sapClientNumber string
+	requestClient   *sap_api_request_client_header_setup.SAPRequestClient
+	log             *logger.Logger
+}
+
+func NewSAPAPICaller(baseUrl, sapClientNumber string, requestClient *sap_api_request_client_header_setup.SAPRequestClient, l *logger.Logger) *SAPAPICaller {
+	return &SAPAPICaller{
+		baseURL:         baseUrl,
+		requestClient:   requestClient,
+		sapClientNumber: sapClientNumber,
+		log:             l,
+	}
+}
+
+func (c *SAPAPICaller) AsyncGetBankMaster(bankCountry, bank string, accepter []string) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(accepter))
+	for _, fn := range accepter {
+		switch fn {
+		case "Bank":
+			func() {
+				c.Bank(bankCountry, bank)
+				wg.Done()
+			}()
+		default:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+}
+
+func (c *SAPAPICaller) Bank(bankCountry, bank string) {
+	data, err := c.callBankSrvAPIRequirementBank("Bank", bankCountry, bank)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+
+}
+
+func (c *SAPAPICaller) callBankSrvAPIRequirementBank(api, bankCountry, bank string) ([]sap_api_output_formatter.Bank, error) {
+	url := strings.Join([]string{c.baseURL, "api_bank/srvd_a2x/sap/bank/0002", api}, "/")
+	param := c.getQueryWithBank(map[string]string{}, bankCountry, bank)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToBank(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) getQueryWithBank(params map[string]string, bankCountry, bank string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("BankCountry eq '%s' and Bank eq '%s'", bankCountry, bank)
+	return params
+}
